f784f94ac3232d05b8c35b786e60212319ab4bbf
Giancarlos Salas <giansalex@gmail.com>
2020-08-23 16:52:30 -0500

    Add rest tx

--- x/scavenge/client/rest/tx.go
+++ x/scavenge/client/rest/tx.go
@@ -1,45 +1,144 @@
 package rest
+
 // The packages below are commented out at first to prevent an error if this file isn't initially saved.
 import (
 	// "bytes"
-	// "net/http"
+	"crypto/sha256"
+	"encoding/hex"
+	"net/http"
 
 	"github.com/gorilla/mux"
 
 	"github.com/cosmos/cosmos-sdk/client/context"
-	// sdk "github.com/cosmos/cosmos-sdk/types"
-	// "github.com/cosmos/cosmos-sdk/types/rest"
-	// "github.com/cosmos/cosmos-sdk/x/auth/client/utils"
-	// "github.com/giansalex/scavenge/x/scavenge/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/rest"
+	"github.com/cosmos/cosmos-sdk/x/auth/client/utils"
+	"github.com/giansalex/scavenge/x/scavenge/types"
 )
 
 func registerTxRoutes(cliCtx context.CLIContext, r *mux.Router) {
-	// r.HandleFunc(
-	// TODO: Define the Rest route ,
-	// Call the function which should be executed for this route),
-	// ).Methods("POST")
+	r.HandleFunc("/scavenge/", getCreateScavenge(cliCtx)).Methods("POST")
+	r.HandleFunc("/scavenge/commit", getCommitScavenge(cliCtx)).Methods("POST")
+	r.HandleFunc("/scavenge/reveal", getRevealScavenge(cliCtx)).Methods("POST")
 }
 
-/*
-// Action TX body
-type <Action>Req struct {
-	BaseReq rest.BaseReq `json:"base_req" yaml:"base_req"`
-	// TODO: Define more types if needed
+type createScavengeReq struct {
+	BaseReq     rest.BaseReq `json:"base_req"`
+	Solution    string       `json:"solution"`
+	Description string       `json:"description"`
+	Reward      string       `json:"reward"`
 }
 
-func <Action>RequestHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
+func getCreateScavenge(cliCtx context.CLIContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var req <Action>Req
-		vars := mux.Vars(r)
+		var req createScavengeReq
+
+		if !rest.ReadRESTReq(w, r, cliCtx.Codec, &req) {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, "failed to parse request")
+			return
+		}
 
 		baseReq := req.BaseReq.Sanitize()
 		if !baseReq.ValidateBasic(w) {
 			return
 		}
 
-		// TODO: Define the module tx logic for this action
+		reward, err := sdk.ParseCoins(req.Reward)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
 
-		utils.WriteGenerateStdTxResponse(w, cliCtx, BaseReq, []sdk.Msg{msg})
+		var solutionHash = sha256.Sum256([]byte(req.Solution))
+		var solutionHashString = hex.EncodeToString(solutionHash[:])
+
+		msg := types.NewMsgCreateScavenge(cliCtx.GetFromAddress(), req.Description, solutionHashString, reward)
+		err = msg.ValidateBasic()
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		utils.WriteGenerateStdTxResponse(w, cliCtx, baseReq, []sdk.Msg{msg})
 	}
 }
-*/
+
+type commitScavengeReq struct {
+	BaseReq  rest.BaseReq `json:"base_req"`
+	Solution string       `json:"solution"`
+}
+
+func getCommitScavenge(cliCtx context.CLIContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req commitScavengeReq
+
+		if !rest.ReadRESTReq(w, r, cliCtx.Codec, &req) {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, "failed to parse request")
+			return
+		}
+
+		baseReq := req.BaseReq.Sanitize()
+		if !baseReq.ValidateBasic(w) {
+			return
+		}
+
+		reward, err := sdk.ParseCoins(req.Reward)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		var solutionHash = sha256.Sum256([]byte(req.Solution))
+		var solutionHashString = hex.EncodeToString(solutionHash[:])
+
+		var scavenger = cliCtx.GetFromAddress().String()
+
+		var solutionScavengerHash = sha256.Sum256([]byte(solution + scavenger))
+		var solutionScavengerHashString = hex.EncodeToString(solutionScavengerHash[:])
+
+		msg := types.NewMsgCommitSolution(cliCtx.GetFromAddress(), solutionHashString, solutionScavengerHashString)
+		err = msg.ValidateBasic()
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		utils.WriteGenerateStdTxResponse(w, cliCtx, baseReq, []sdk.Msg{msg})
+	}
+}
+
+type revealScavengeReq struct {
+	BaseReq  rest.BaseReq `json:"base_req"`
+	Solution string       `json:"solution"`
+}
+
+func getRevealScavenge(cliCtx context.CLIContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req revealScavengeReq
+
+		if !rest.ReadRESTReq(w, r, cliCtx.Codec, &req) {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, "failed to parse request")
+			return
+		}
+
+		baseReq := req.BaseReq.Sanitize()
+		if !baseReq.ValidateBasic(w) {
+			return
+		}
+
+		reward, err := sdk.ParseCoins(req.Reward)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		msg := types.NewMsgRevealSolution(cliCtx.GetFromAddress(), req.Solution)
+		err = msg.ValidateBasic()
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		utils.WriteGenerateStdTxResponse(w, cliCtx, baseReq, []sdk.Msg{msg})
+	}
+}
