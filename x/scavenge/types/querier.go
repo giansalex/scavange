225e6c47147820fd1b6922910abf61607179e00c
Giancarlos Salas <giansalex@gmail.com>
2020-08-23 14:20:35 -0500

    Add querier

--- x/scavenge/types/querier.go
+++ x/scavenge/types/querier.go
@@ -1,13 +1,15 @@
 package types
 
+import "strings"
 
-// Query endpoints supported by the scavenge querier
+// query endpoints supported by the nameservice Querier
 const (
-	// TODO: Describe query parameters, update <action> with your query
-	// Query<Action>    = "<action>"
+	QueryListScavenges = "list"
+	QueryGetScavenge   = "get"
+	QueryCommit        = "commit"
 )
 
-/* 
+/*
 Below you will be able how to set your own queries:
 
 
@@ -20,3 +22,11 @@ func (n QueryResList) String() string {
 }
 
 */
+
+// QueryResScavenges Queries Result Payload for a names query
+type QueryResScavenges []string
+
+// implement fmt.Stringer
+func (n QueryResScavenges) String() string {
+	return strings.Join(n[:], "\n")
+}
