80f5da4931f04da2b2f26e7c888738ca84285d30
Giancarlos Salas <giansalex@gmail.com>
2020-08-23 13:59:31 -0500

    Add keeper

--- x/scavenge/keeper/keeper.go
+++ x/scavenge/keeper/keeper.go
@@ -3,27 +3,27 @@ package keeper
 import (
 	"fmt"
 
-	"github.com/tendermint/tendermint/crypto"
 	"github.com/tendermint/tendermint/libs/log"
 
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/bank"
 	"github.com/giansalex/scavenge/x/scavenge/types"
 )
 
 // Keeper of the scavenge store
 type Keeper struct {
+	CoinKeeper bank.Keeper
 	storeKey   sdk.StoreKey
 	cdc        *codec.Codec
-	paramspace types.ParamSubspace
 }
 
 // NewKeeper creates a scavenge keeper
-func NewKeeper(cdc *codec.Codec, key sdk.StoreKey, paramspace types.ParamSubspace) Keeper {
+func NewKeeper(coinKeeper bank.Keeper, cdc *codec.Codec, key sdk.StoreKey) Keeper {
 	keeper := Keeper{
+		CoinKeeper: coinKeeper,
 		storeKey:   key,
 		cdc:        cdc,
-		paramspace: paramspace.WithKeyTable(types.ParamKeyTable()),
 	}
 	return keeper
 }
@@ -33,25 +33,62 @@ func (k Keeper) Logger(ctx sdk.Context) log.Logger {
 	return ctx.Logger().With("module", fmt.Sprintf("x/%s", types.ModuleName))
 }
 
-// Get returns the pubkey from the adddress-pubkey relation
-func (k Keeper) Get(ctx sdk.Context, key string) (/* TODO: Fill out this type */, error) {
+// GetCommit returns the commit of a solution
+func (k Keeper) GetCommit(ctx sdk.Context, solutionScavengerHash string) (types.Commit, error) {
 	store := ctx.KVStore(k.storeKey)
-	var item /* TODO: Fill out this type */
-	byteKey := []byte(key)
-	err := k.cdc.UnmarshalBinaryLengthPrefixed(store.Get(byteKey), &item)
+	var commit types.Commit
+	byteKey := []byte(types.CommitPrefix + solutionScavengerHash)
+	err := k.cdc.UnmarshalBinaryLengthPrefixed(store.Get(byteKey), &commit)
 	if err != nil {
-		return nil, err
+		return commit, err
 	}
-	return item, nil
+	return commit, nil
 }
 
-func (k Keeper) set(ctx sdk.Context, key string, value /* TODO: fill out this type */ ) {
+// GetScavenge returns the scavenge information
+func (k Keeper) GetScavenge(ctx sdk.Context, solutionHash string) (types.Scavenge, error) {
 	store := ctx.KVStore(k.storeKey)
-	bz := k.cdc.MustMarshalBinaryLengthPrefixed(value)
-	store.Set([]byte(key), bz)
+	var scavenge types.Scavenge
+	byteKey := []byte(types.ScavengePrefix + solutionHash)
+	err := k.cdc.UnmarshalBinaryLengthPrefixed(store.Get(byteKey), &scavenge)
+	if err != nil {
+		return scavenge, err
+	}
+	return scavenge, nil
 }
 
-func (k Keeper) delete(ctx sdk.Context, key string) {
+// SetCommit sets a scavenge
+func (k Keeper) SetCommit(ctx sdk.Context, commit types.Commit) {
+	solutionScavengerHash := commit.SolutionScavengerHash
 	store := ctx.KVStore(k.storeKey)
-	store.Delete([]byte(key))
+	bz := k.cdc.MustMarshalBinaryLengthPrefixed(commit)
+	key := []byte(types.CommitPrefix + solutionScavengerHash)
+	store.Set(key, bz)
+}
+
+// SetScavenge sets a scavenge
+func (k Keeper) SetScavenge(ctx sdk.Context, scavenge types.Scavenge) {
+	solutionHash := scavenge.SolutionHash
+	store := ctx.KVStore(k.storeKey)
+	bz := k.cdc.MustMarshalBinaryLengthPrefixed(scavenge)
+	key := []byte(types.ScavengePrefix + solutionHash)
+	store.Set(key, bz)
+}
+
+// DeleteScavenge deletes a scavenge
+func (k Keeper) DeleteScavenge(ctx sdk.Context, solutionHash string) {
+	store := ctx.KVStore(k.storeKey)
+	store.Delete([]byte(solutionHash))
+}
+
+// GetScavengesIterator gets an iterator over all scavnges in which the keys are the solutionHashes and the values are the scavenges
+func (k Keeper) GetScavengesIterator(ctx sdk.Context) sdk.Iterator {
+	store := ctx.KVStore(k.storeKey)
+	return sdk.KVStorePrefixIterator(store, []byte(types.ScavengePrefix))
+}
+
+// GetCommitsIterator gets an iterator over all commits in which the keys are the prefix and solutionHashes and the values are the scavenges
+func (k Keeper) GetCommitsIterator(ctx sdk.Context) sdk.Iterator {
+	store := ctx.KVStore(k.storeKey)
+	return sdk.KVStorePrefixIterator(store, []byte(types.CommitPrefix))
 }
