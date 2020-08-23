ffd142145734a1782c4a8dbc940326f39f9b7178
Giancarlos Salas <giansalex@gmail.com>
2020-08-23 12:05:18 -0500

    Complete msg create

--- x/scavenge/types/MsgCreateScavenge.go
+++ x/scavenge/types/MsgCreateScavenge.go
@@ -6,42 +6,51 @@ import (
 )
 
 // TODO: Describe your actions, these will implment the interface of `sdk.Msg`
-/*
-// verify interface at compile time
-var _ sdk.Msg = &Msg<Action>{}
 
-// Msg<Action> - struct for unjailing jailed validator
-type Msg<Action> struct {
-	ValidatorAddr sdk.ValAddress `json:"address" yaml:"address"` // address of the validator operator
+// MsgCreateScavenge
+var _ sdk.Msg = &MsgCreateScavenge{}
+
+// MsgCreateScavenge - struct for unjailing jailed validator
+type MsgCreateScavenge struct {
+	Creator      sdk.AccAddress `json:"creator" yaml:"creator"`           // address of the scavenger creator
+	Description  string         `json:"description" yaml:"description"`   // description of the scavenge
+	SolutionHash string         `json:"solutionHash" yaml:"solutionHash"` // solution hash of the scavenge
+	Reward       sdk.Coins      `json:"reward" yaml:"reward"`             // reward of the scavenger
 }
 
-// NewMsg<Action> creates a new Msg<Action> instance
-func NewMsg<Action>(validatorAddr sdk.ValAddress) Msg<Action> {
-	return Msg<Action>{
-		ValidatorAddr: validatorAddr,
+// NewMsgCreateScavenge creates a new MsgCreateScavenge instance
+func NewMsgCreateScavenge(creator sdk.AccAddress, description, solutionHash string, reward sdk.Coins) MsgCreateScavenge {
+	return MsgCreateScavenge{
+		Creator:      creator,
+		Description:  description,
+		SolutionHash: solutionHash,
+		Reward:       reward,
 	}
 }
 
-const <action>Const = "<action>"
+// CreateScavengeConst is CreateScavenge Constant
+const CreateScavengeConst = "CreateScavenge"
 
 // nolint
-func (msg Msg<Action>) Route() string { return RouterKey }
-func (msg Msg<Action>) Type() string  { return <action>Const }
-func (msg Msg<Action>) GetSigners() []sdk.AccAddress {
-	return []sdk.AccAddress{sdk.AccAddress(msg.ValidatorAddr)}
+func (msg MsgCreateScavenge) Route() string { return RouterKey }
+func (msg MsgCreateScavenge) Type() string  { return CreateScavengeConst }
+func (msg MsgCreateScavenge) GetSigners() []sdk.AccAddress {
+	return []sdk.AccAddress{sdk.AccAddress(msg.Creator)}
 }
 
 // GetSignBytes gets the bytes for the message signer to sign on
-func (msg Msg<Action>) GetSignBytes() []byte {
+func (msg MsgCreateScavenge) GetSignBytes() []byte {
 	bz := ModuleCdc.MustMarshalJSON(msg)
 	return sdk.MustSortJSON(bz)
 }
 
 // ValidateBasic validity check for the AnteHandler
-func (msg Msg<Action>) ValidateBasic() error {
-	if msg.ValidatorAddr.Empty() {
-		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "missing validator address")
+func (msg MsgCreateScavenge) ValidateBasic() error {
+	if msg.Creator.Empty() {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "creator can't be empty")
+	}
+	if msg.SolutionHash == "" {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "solutionScavengerHash can't be empty")
 	}
 	return nil
 }
-*/
